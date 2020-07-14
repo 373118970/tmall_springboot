cda0223e3cd66b264e55be4a3e1ac8404b7fc628
沐白白 <373118970@qq.com>
2020-07-14 18:48:34 +0800

    初始提交版本

---
+++ src/main/java/com/how2java/tmall/pojo/Category.java
@@ -0,0 +1,23 @@
+package com.how2java.tmall.pojo;
+
+public class Category {
+    int id;
+
+    String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
